e434335aefd573ded3436358112ecbc331f9c25e
Fran Bastos <francielydbastos@gmail.com>
2024-11-14 10:45:46 -0300

    Creating UserController and implementing controller advice for handling exceptions

---
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ead.authuser.services.impl;
+
+import com.ead.authuser.exceptions.NotFoundException;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserRepository;
+import com.ead.authuser.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+
+    final UserRepository userRepository;
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        Optional<UserModel> userModelOptional = userRepository.findById(userId);
+
+        if(userModelOptional.isEmpty()) {
+            throw new NotFoundException("Error: User not found");
+        }
+
+        return userModelOptional;
+    }
+
+
+    @Override
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
+}
