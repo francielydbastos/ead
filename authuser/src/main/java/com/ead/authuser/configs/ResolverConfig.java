f493ff59f660b2a60f7fb315aa0579c0e27ad180
Fran Bastos <francielydbastos@gmail.com>
2024-12-01 18:07:34 -0300

    Adding CORS configuration

--- authuser/src/main/java/com/ead/authuser/configs/ResolverConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/ResolverConfig.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,6 +19,13 @@ public class ResolverConfig implements WebMvcConfigurer {
         pageableResolver.setFallbackPageable(PageRequest.of(0, 10, Sort.Direction.ASC));
 
         argumentResolvers.add(pageableResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/users/**").allowedOrigins("*");
+        registry.addMapping("/auth/**").allowedOrigins("*");
+
     }
 }
 
