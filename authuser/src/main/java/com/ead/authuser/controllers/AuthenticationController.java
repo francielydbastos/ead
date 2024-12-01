d7bd5d4875f264537492947ad50c5d3d74cc7221
Fran Bastos <francielydbastos@gmail.com>
2024-12-01 15:49:49 -0300

    Creating methods and endpoints to update user details, password and image.

---
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.dtos.UserRecordDto;
+import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> registerUser(@RequestBody @JsonView(UserRecordDto.UserView.RegistrationPost.class) UserRecordDto userRecordDto) {
+        if(userService.existsByUsername(userRecordDto.username())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is already in use.");
+        }
+        if(userService.existsByEmail(userRecordDto.email())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is already in use.");
+        }
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(userRecordDto));
+    }
+
+
+}
--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,13 +1,16 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -31,6 +34,33 @@ public class UserController {
     public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId) {
         userService.delete(userService.findById(userId).get());
         return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully.");
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Object> updateUser(@PathVariable(value = "userId") UUID userId, @RequestBody @JsonView(UserRecordDto.UserView.UserPut.class) UserRecordDto userRecordDto) {
+        var userModelOptional = userService.findById(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(userRecordDto, userModelOptional.get()));
+
+    }
+
+    @PutMapping("/{userId}/password")
+    public ResponseEntity<Object> updatePassword(@PathVariable(value = "userId") UUID userId, @RequestBody @JsonView(UserRecordDto.UserView.PasswordPut.class) UserRecordDto userRecordDto) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if(!userModelOptional.get().getPassword().equals(userRecordDto.oldPassword())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Old password does not match.");
+        }
+
+        userService.updatePassword(userRecordDto, userModelOptional.get());
+
+        return ResponseEntity.status(HttpStatus.OK).body("Password updated successfully!");
+    }
+
+    @PutMapping("/{userId}/image")
+    public ResponseEntity<Object> updateImage(@PathVariable(value = "userId") UUID userId, @RequestBody @JsonView(UserRecordDto.UserView.ImagePut.class) UserRecordDto userRecordDto) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateImage(userRecordDto, userModelOptional.get()));
+
     }
 
 }
