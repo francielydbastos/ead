127ac152882864876c0fc5b6e1ed16aa7e19fe4e
Fran Bastos <francielydbastos@gmail.com>
2024-12-01 17:29:27 -0300

    COnfiguring pagination with global configuration

--- authuser/src/main/java/com/ead/authuser/services/UserService.java
+++ authuser/src/main/java/com/ead/authuser/services/UserService.java
@@ -2,6 +2,8 @@ package com.ead.authuser.services;
 
 import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,4 +29,5 @@ public interface UserService {
 
     UserModel updateImage(UserRecordDto userRecordDto, UserModel userModel);
 
+    Page<UserModel> findAll(Pageable pageable);
 }
