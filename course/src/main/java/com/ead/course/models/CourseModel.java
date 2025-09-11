8ddde3b9b534d566993b524c2dff9681b4e30316
francielydbastos <francielydbastos@gmail.com>
2025-09-11 13:24:44 -0300

    course model jpa mapping

---
+++ course/src/main/java/com/ead/course/models/CourseModel.java
@@ -0,0 +1,60 @@
+package com.ead.course.models;
+
+
+import com.ead.course.enums.CourseLevel;
+import com.ead.course.enums.CourseStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Entity
+@Table(name ="TB_COURSES")
+public class CourseModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID courseId;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime lastUpdateDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CourseStatus courseStatus;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CourseLevel courseLevel;
+
+    @Column(nullable = false)
+    private UUID userInstructor;
+
+    @Column
+    private String imageUrl;
+}
